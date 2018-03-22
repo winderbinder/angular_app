f1ed1b4309181e7cab14582a779c2597bddd0cb0
winderbinder <junior_19000@hotmail.com>
2018-03-22 15:07:12 -0600

    added sample data for rails microapp

--- freelance_camp_documents/db/seeds.rb
+++ freelance_camp_documents/db/seeds.rb
@@ -1,7 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#   create_table "freelance_documents", force: :cascade do |t|
+#     t.string "title"
+#     t.string "description"
+#     t.text "file_url"
+#     t.text "image_url"
+#     t.datetime "created_at", null: false
+#     t.datetime "updated_at", null: false
+#   end
+
+# end
+
+10.times do |copy|
+FreelanceDocument.create!(title: "Document #{copy}",
+                          description: "though sahlt an die fhaiw goeur as ciehfo coif aieirf ofoha foeoro oaoc weifba gboier oaef oc eori coiuae aoif erero",
+                          file_url: "https://docs.google.com/document/d/1UUgAob6ZpivgkgCbMD84JMMRc1flrzCPKybtQoAfASo/edit",
+                          image_url: "http://www.bigfoto.com/stones-background.jpg"
+  )
+end
+puts "10 documents created"
