28f27b31a3a8a7b7b827c7050b30463919505d40
winderbinder <junior_19000@hotmail.com>
2018-03-23 10:39:07 -0600

    wired the application to be able to submit and recieve data

--- freelance-camp-fe/app/proposal/proposal-new.component.js
+++ freelance-camp-fe/app/proposal/proposal-new.component.js
@@ -5,21 +5,37 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
+var Rx_1 = require("rxjs/Rx");
 var proposal_1 = require("./proposal");
+var proposal_service_1 = require("./proposal.service");
 var ProposalNewComponent = /** @class */ (function () {
-    function ProposalNewComponent() {
+    function ProposalNewComponent(proposalService) {
+        this.proposalService = proposalService;
         this.proposal = new proposal_1.Proposal;
         this.submitted = false;
     }
+    ProposalNewComponent.prototype.createProposal = function (proposal) {
+        this.submitted = true;
+        this.proposalService.createProposal(proposal)
+            .subscribe(function (data) { return true; }, function (error) {
+            console.log("error saving proposal");
+            return Rx_1.Observable.throw(error);
+        });
+    };
     ProposalNewComponent = __decorate([
         core_1.Component({
             moduleId: module.id,
             selector: 'proposal-new',
             templateUrl: 'proposal-new.component.html',
-            styleUrls: ['proposal-new.component.css']
-        })
+            styleUrls: ['proposal-new.component.css'],
+            providers: [proposal_service_1.ProposalService]
+        }),
+        __metadata("design:paramtypes", [proposal_service_1.ProposalService])
     ], ProposalNewComponent);
     return ProposalNewComponent;
 }());
